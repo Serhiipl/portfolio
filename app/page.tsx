00ba7390b490e95402e1c747b2d0bb4533366199
Sergio_Mac <badzega@gmail.com>
2024-03-21 22:05:05 +0100

    Add projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import About from "@/components/About";
 import Divider from "@/components/Divider";
 import Hero from "@/components/Hero";
-import Projects from "@/components/projects";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,38 @@
 "use client";
 
+import { projectsData } from "@/lib/data";
 import SectionHeader from "./SectionHeader";
 import React from "react";
+import Image from "next/image";
 
 export default function Projects() {
   return (
     <section>
-      <SectionHeader>Projects</SectionHeader>
-      <div></div>
+      <SectionHeader>my Projects</SectionHeader>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} alt="Project" width={200} height={200} />
     </section>
   );
 }
