691675cc532847368bae5e3580706f4e8d44d41f
Sergio_Mac <badzega@gmail.com>
2024-03-21 14:16:04 +0100

    CREATE: Projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Divider from "@/components/Divider";
 import Hero from "@/components/Hero";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Divider />
       <About />
+      <Projects />
     </main>
   );
 }
---
+++ components/Projects.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SectionHeader from "./SectionHeader";
+import React from "react";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeader>Projects</SectionHeader>
+      <div></div>
+    </section>
+  );
+}
