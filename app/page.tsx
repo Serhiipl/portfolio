e872e9385d897246790f4a7a142c16a5b7b82215
Sergio_Mac <badzega@gmail.com>
2024-03-21 11:46:35 +0100

    ADD: section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Divider from "@/components/Divider";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
+      <Divider />
     </main>
   );
 }
---
+++ components/Divider.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Divider() {
+  return (
+    <motion.div
+      className="bg-gray-200 my-24 w-1 h-16 rounded-full hidden sm:block"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+    ></motion.div>
+  );
+}
