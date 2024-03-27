a9fde714397a7638fd172dac4cf0bd09149e232f
Sergio_Mac <badzega@gmail.com>
2024-03-27 21:30:21 +0100

    fix problem width ReactVertical-timeLine

---
+++ components/Experience.jsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SectionHeader from "./SectionHeader";
+import { experiencesData } from "@/lib/data";
+
+export default function Experience() {
+  const { ref, inView } = useInView({ triggerOnce: false });
+  return (
+    <section id="experience" ref={ref}>
+      <SectionHeader>My Experience</SectionHeader>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              visible={inView}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0,0,0,0.05)",
+                textAlign: "left",
+                padding: "1rem 3 rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
