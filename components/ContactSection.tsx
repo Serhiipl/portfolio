997c41e120e9228530d744eac419bf3f34608887
Sergio_Mac <badzega@gmail.com>
2024-04-02 08:50:04 +0200

    add motion to contact section

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import SectionHeader from "./SectionHeader";
+import { motion } from "framer-motion";
 
 export default function ContactSection() {
   return (
-    <div>
+    <motion.section
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)]"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+    >
       <SectionHeader>Contact</SectionHeader>
-    </div>
+    </motion.section>
   );
 }
